4349197c263a7601b565478b254c0e2fe109254d
Sreeharsha Sistla <sreeharsha.sistla@gmail.com>
2025-10-17 18:00:43 -0700

    initial pages

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+// Components
+import ComponentLocator from './components/ComponentLocator';
+import ComponentSearchResults from './components/ComponentSearchResults';
+import { AuthProvider } from './contexts/AuthContext';
+
+function Layout({ children }) {
+  //const location = useLocation();
+  const dontShowHeaderAndFooter = true; // location.pathname?.includes("raw");
+  return (
+    <div className="App d-flex flex-column min-vh-100" style={{ maxWidth: '90%', margin: 'auto' }}>
+      {!dontShowHeaderAndFooter && <Header />}
+      <main className="flex-grow-1">{children}</main>
+      {!dontShowHeaderAndFooter && <Footer />}
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <Layout>
+            <Routes>
+              <Route path="/" element={<ComponentLocator />} />
+              <Route path="/search" element={<ComponentSearchResults />} />
+            </Routes>
+        </Layout>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
